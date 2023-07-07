c3ed546c33418634247d88e7fdcc6f5d9b929fb8
mayank <mayankrm2003@gmail.com>
2023-07-08 02:17:22 +0530

    added app

---
+++ app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+
+
+
+def main():
+    st.title("Code of Conduct")
+
+    st.header("1. Respect and Inclusivity")
+    st.markdown("""
+    - Treat all community members with respect, kindness, and empathy, regardless of their background, gender identity and expression, sexual orientation, disability, neurodivergence, personal appearance, race, ethnicity, age, religion, nationality, or other similar characteristics.
+    - Engage in constructive and respectful discussions, focusing on the topic at hand while being mindful of diverse perspectives.
+    - Avoid discriminatory or offensive language, including but not limited to racism, homophobia, hate speech, and derogatory comments.
+    """)
+
+    st.header("2. Safe and Welcoming Environment")
+    st.markdown("""
+    - Create a safe and welcoming environment for all community members, free from harassment, bullying, or intimidation.
+    - Do not engage in personal attacks, targeted harassment, or name-calling.
+    - Be mindful of the potential impact of your words and actions on others, and strive to maintain a positive atmosphere.
+    """)
+
+    st.header("3. Relevant and Informative Discussions")
+    st.markdown("""
+    - Engage in discussions that are relevant to the holistic healing and well-being focus of our community.
+    - Share resources, information, and personal experiences that contribute to the understanding and practice of holistic healing.
+    - Avoid discussing unrelated topics that may be divisive or harmful, such as illegal activities, politics, religion, relationships, mental health, medical conditions, medicine, homeopathic remedies, etc.
+    """)
+
+    st.header("4. Collaborative Learning and Support")
+    st.markdown("""
+    - Support and encourage each other's growth and learning journeys.
+    - Share insights, knowledge, and experiences related to holistic healing and well-being in a helpful and informative manner.
+    - Guide others by providing practical advice and suggestions, empowering them to find their own solutions.
+    - Respect personal boundaries and seek consent before initiating direct messages or private conversations.
+    """)
+
+    st.header("5. Detailed Questions and Information Sharing")
+    st.markdown("""
+    - When seeking assistance, provide detailed questions and context to make it easier for others to understand and offer relevant guidance.
+    - Avoid asking low-effort questions that can be easily googled or solved with minimal effort.
+    - Share relevant resources, articles, studies, or experiences related to holistic healing and well-being, providing context and explanations to facilitate meaningful discussions.
+    """)
+
+    st.header("6. Reporting and Addressing Issues")
+    st.markdown("""
+    - Report any misconduct, harassment, or behavior that violates the Code of Conduct to the moderators through the designated reporting channels.
+    - Avoid engaging in confrontations or attempting to correct misconduct directly.
+    - Trust the moderators to handle reported issues and maintain a fair and safe environment for all.
+    """)
+
+    st.header("7. Professionalism and Ethical Behavior")
+    st.markdown("""
+    - Act professionally, maintain integrity, and adhere to ethical principles within the community.
+    - Refrain from engaging in activities that may be considered unethical, illegal, or harmful, such as piracy, plagiarism, doxxing, or promoting fraudulent practices.
+    - Be mindful of the potential impact of your advice and recommendations on others' health and well-being, and ensure they align with ethical standards.
+    """)
+
+    st.markdown("""Going against any codes mentioned here will result in a warning, if any member receives 3 such warnings, they will cease to be part of this commmunity and permanently banned from the server""")
+
+    st.header("Terms of Conditions")
+    st.warning("""
+    *Warning:* The information shared in this community is for educational and informational purposes only. It is not intended to replace professional medical advice, diagnosis, or treatment. Each individual is responsible for their own health and well-being.
+    
+    While the practices, exercises, and suggestions shared in the community are meant to be safe and within the boundaries of well-being, it is important to consult with qualified professionals before making any significant changes to your health routines.
+    
+    The moderators, administrators, and members of this community are not responsible for any harm, injury, or negative outcomes that may occur as a result of following or implementing suggestions from other Discord users or information shared within the community. Members should exercise caution and use their own discretion when applying any practices or advice discussed in the community.
+    """)
+
+if __name__ == '__main__':
+    main()
